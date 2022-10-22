29c00335c070f0484ee03447b11487b3f881a077
Sergey <mulichev@gmail.com>
2022-10-22 14:08:24 +0300

    weather theme block moved to WeatherDisplay class

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -7,17 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import com.example.weather.presentation.ui.theme.DarkBlue
-import com.example.weather.presentation.ui.theme.DeepBlue
 import com.example.weather.presentation.ui.theme.WeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,6 +23,7 @@ class MainActivity : ComponentActivity() {
         permissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) {
+            viewModel.isFirstLaunch = true
             viewModel.loadAllInfo()
         }
 
@@ -44,36 +36,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             WeatherTheme {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(DarkBlue)
-                    ) {
-                        WeatherCard(
-                            state = viewModel.state,
-                            localityName = viewModel.localityName,
-                            backgroundColor = DeepBlue
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        WeatherForecast(state = viewModel.state)
-                    }
-
-                    if (viewModel.state.isLoading) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                    viewModel.state.error?.let { error ->
-                        Text(
-                            text = error,
-                            color = Color.Red,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                        )
-                    }
-                }
+                WeatherDisplay(
+                    state = viewModel.state,
+                    localityName = viewModel.localityName,
+                    isRefreshing = viewModel.isRefreshing,
+                    onRefresh = {
+                        viewModel.loadAllInfo()
+                    },
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
