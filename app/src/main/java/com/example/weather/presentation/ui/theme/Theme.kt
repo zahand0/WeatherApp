c1523fc0b9ce8fdd2ca828f64fe124c9dea259e2
Sergey <mulichev@gmail.com>
2022-10-22 14:06:29 +0300

    added dark/light mode

--- app/src/main/java/com/example/weather/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/weather/presentation/ui/theme/Theme.kt
@@ -6,9 +6,32 @@ import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = DarkBlue,
+    background = DeepBlue,
+    onBackground = Color.White,
+    surface = Color.DarkGray,
+    onSurface = Color.LightGray
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color(0xFFADB8CB),
+    background = Color(0xFFE9EDF3),
+    onBackground = Color(0xFF394957),
+    surface = Color.LightGray,
+    onSurface = Color.DarkGray
+)
 
 @Composable
-fun WeatherTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun WeatherTheme(nightTime: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (nightTime) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
     MaterialTheme(
         colors = colors,
         typography = Typography,
